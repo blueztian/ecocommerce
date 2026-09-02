158404d0c72e142ad89b5fe5e4a5137b2bf8ef5f
blueztian <166921335+blueztian@users.noreply.github.com>
2026-09-02 22:27:13 +0800

    Link batch3.css and add skip-to-content link

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="assets/css/cart.css">
     <link rel="stylesheet" href="assets/css/enhancements.css">
+    <link rel="stylesheet" href="assets/css/batch3.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Nunito+Sans:wght@400;600;700&family=Bangers&display=swap" rel="stylesheet">
@@ -23,3 +24,4 @@
 </head>
 
 <body id="top">
+<a href="#shop" class="skip-link">Skip to main content</a>
